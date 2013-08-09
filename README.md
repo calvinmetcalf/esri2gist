f7b3a32a35f58e059f097c311e963d5f7783bce4
Calvin Metcalf <calvin.metcalf@state.ma.us>
2013-08-09 10:45:26 -0400

    first

--- README.md
+++ README.md
@@ -1,10 +1,7 @@
-esri2couch
+esri2gist
 ==========
-takes a file, a url for a couchdb,  the location of your
-[esri2open](https://github.com/project-open-data/esri2open) toolbox and an
-optional login of the form username:password.  If you use that your going
-to want to make sure to put https in the name of your couch. There is a
-box you can check to create the DB it only does anything if you've filled in
-your authentication. A design document is created with a preview of your layer
-viewable at http://your.couch.com/dbname/_design/geojson/_rewrite/ viewing this for
-larger layers can crash your browsers FYI
+takes a file, the location of your
+[esri2open](https://github.com/project-open-data/esri2open) toolbox, an
+optional github of the form username:password, and an optional description.
+
+Makes a Gist. Check the results panel for the the url (or if you logged in you can check your gists)
---
+++ gist.py
@@ -0,0 +1,53 @@
+import arcpy
+from tempfile import mkdtemp
+from os import remove, rmdir,path, sep
+from urllib2 import Request, urlopen
+from base64 import b64encode
+from json import dumps,loads
+
+def getName(feature):
+    if feature[0] in ("'",'"'):
+        feature = feature[1:-1]
+    name = path.splitext(path.split(feature)[1])
+    if name[1]:
+        if name[1]==".shp":
+            return name[0]
+        else:
+            return name[1][1:]
+    else:
+        return name[0]
+
+def doStuff(inFile,tbx,login,description):
+    arcpy.ImportToolbox(tbx)
+    base = mkdtemp()
+    outFile = base+'//output.geojson'
+    arcpy.esri2open(inFile,outFile)
+    postGist(outFile,getName(inFile),login,description)
+    cleanUp(base,outFile)
+
+def cleanUp(p,f):
+    remove(f)
+    rmdir(p)
+    
+
+def dealLogin(req,login):
+    if login == 'NONE':
+        return
+    req.add_header('Authorization','Basic '+b64encode(login))
+
+def postGist(outFile,filename,login,description):
+    newUrl = 'https://api.github.com/gists'
+    baseFile = open(outFile,'r')
+    data = {"public":True,"files":{filename+".geojson":{'content':baseFile.read()}}}
+    baseFile.close()
+    if description:
+        data['description']=description
+    req = Request(newUrl)
+    req.add_header('Content-Type', 'application/json')
+    dealLogin(req,login)
+    resp = urlopen(req,dumps(data))
+    r = loads(resp.read())
+    url = 'https://gist.github.com'+r['url'][r['url'].rfind('/'):]
+    arcpy.Addmessage(url)
+
+doStuff(arcpy.GetParameterAsText(0),arcpy.GetParameterAsText(1),arcpy.GetParameterAsText(2),arcpy.GetParameterAsText(3))
